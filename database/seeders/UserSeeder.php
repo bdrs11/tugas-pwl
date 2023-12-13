4dba71755acbacc91cce266c6e5ea4d568ff771b
unknown <sbadrus620@gmail.com>
2023-12-13 16:05:49 +0700

    modul 4

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //pengisian data ke dalam table user
+        DB::table('users')->insert([
+            'name' => 'Badruss',
+            'email' => 'salam@gmail.com',
+            'password' => Hash::make('123456789'),
+        ]);
+    }
+}
